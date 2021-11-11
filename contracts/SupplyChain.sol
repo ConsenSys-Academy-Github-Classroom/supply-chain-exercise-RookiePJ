ad407c254df5ef5381c5e950d555def4b36f2d35
Peter Rooke <peter.rooke.work@gmail.com>
2021-11-11 12:34:10 +0000

    Added data storage variables.  Variable tests now passing

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -4,14 +4,20 @@ pragma solidity >=0.5.16 <0.9.0;
 contract SupplyChain {
 
   // <owner>
+  address public owner;
 
   // <skuCount>
+  uint public skuCount;
 
   // <items mapping>
+  mapping (uint => Item) items;
 
   // <enum State: ForSale, Sold, Shipped, Received>
+  enum State { ForSale, Sold, Shipped, Received }
 
   // <struct Item: name, sku, price, state, seller, and buyer>
+  struct Item { string name; uint sku; uint price; State state; address payable seller; address payable buyer; }
+
   
   /* 
    * Events
